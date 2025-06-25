68828b10d1b10568482af0e7d63325d93e46f548
Rahxephonz <mynev.id@gmail.com>
2025-06-25 11:20:30 +0700

    feat: initialize repository for veln chat built with desktop and web platform

---
+++ src/app/layout.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { Inter_Tight } from "next/font/google";
+import "@/styles/globals.css";
+
+const interTight = Inter_Tight({
+  variable: "--font-inter-tight",
+  subsets: ["latin"],
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${interTight.variable} antialiased`}>{children}</body>
+    </html>
+  );
+}
